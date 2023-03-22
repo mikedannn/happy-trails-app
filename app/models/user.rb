bb98400132905353b51fa293ab9d76b9ab959e74
Michael <michael.daniels1013@gmail.com>
2023-03-21 23:53:37 -0500

    Stopping point

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :role, presence: true
+
+    # has_many: user_trails
+    # has_many: trails, through: :user_trails
+    # has_many: resorts, through: :trails
 end
